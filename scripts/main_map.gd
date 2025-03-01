b13936985d52124aec5fffbc6a4634bb13c7fe83
Melissa <fowlerma32@gmail.com>
2025-03-01 16:57:56 -0500

    Updated transitions to allow transitioning to all maps.

--- scripts/main_map.gd
+++ scripts/main_map.gd
@@ -3,11 +3,36 @@ extends Node
 func _ready() -> void:
 	$Player.position = Global.spawn_position  # Set player position
 
-func _on_area_2d_body_entered(body: Node2D) -> void:
+
+func _on_main_to_forest_transition_body_entered(body: Node2D) -> void:
 	if body is Player:
+		Global.transition_scene = true
+		var scene_path = "res://scenes/forest_map.tscn"
 		Global.spawn_position = Vector2(161, -29)
-		call_deferred("change_scene", "res://scenes/forest_map.tscn")
+		Global.current_scene = "Forest"
+		call_deferred("change_scene",scene_path)
+
+
+func _on_main_to_forest_transition_body_exited(body: Node2D) -> void:
+	if body is Player:
+		Global.transition_scene = false
+
+
+func _on_main_to_mountain_transition_body_entered(body: Node2D) -> void:
+	if body is Player:
+		Global.transition_scene = true
+		var scene_path = "res://scenes/mountain_map.tscn"
+		Global.spawn_position = Vector2(162, 245)
+		Global.current_scene = "Mountain"
+		call_deferred("change_scene",scene_path)
+
+
+func _on_main_to_mountain_transition_body_exited(body: Node2D) -> void:
+	if body is Player:
+		Global.transition_scene = false
 
 
 func change_scene(scene_path: String) -> void:
-	get_tree().change_scene_to_file(scene_path)
+	if Global.transition_scene == true:
+		get_tree().change_scene_to_file(scene_path)
+		Global.finish_change_scene()
--- scripts/mountain_map.gd
+++ scripts/mountain_map.gd
@@ -1,13 +1,22 @@
 extends Node
 
-
-func _on_area_2d_body_entered(body: Node2D) -> void:
-	if body is Player:
-		Global.spawn_position = Vector2(-405, 236)
-		call_deferred("change_scene")
-
-func change_scene() -> void:
-	get_tree().change_scene_to_file("res://scenes/main_map.tscn")
-	
 func _ready():
 	$Player.position = Global.spawn_position  # Set player position
+
+func _on_mountain_to_main_transition_body_entered(body: Node2D) -> void:
+	if body is Player:
+		Global.transition_scene = true
+		var scene_path = "res://scenes/main_map.tscn"
+		Global.spawn_position = Vector2(-455, -244)
+		Global.current_scene = "MainMap"
+		call_deferred("change_scene",scene_path)
+
+
+func _on_mountain_to_main_transition_body_exited(body: Node2D) -> void:
+	if body is Player:
+		Global.transition_scene = false
+
+func change_scene(scene_path: String) -> void:
+	if Global.transition_scene == true:
+		get_tree().change_scene_to_file(scene_path)
+		Global.finish_change_scene()
