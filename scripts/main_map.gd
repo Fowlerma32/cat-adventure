f7492b84c2e1974003d4e6e9eb7d67b3d5f77b5a
Melissa <fowlerma32@gmail.com>
2025-03-01 14:50:01 -0500

    Created animated sprite. Added borders to the maps. Working on second map transition.

--- scripts/main_map.gd
+++ scripts/main_map.gd
@@ -1,12 +1,13 @@
 extends Node
 
+func _ready() -> void:
+	$Player.position = Global.spawn_position  # Set player position
+
 func _on_area_2d_body_entered(body: Node2D) -> void:
 	if body is Player:
-		Global.spawn_position = Vector2(161, -29)  # Set spawn position
-		call_deferred("change_scene")
+		Global.spawn_position = Vector2(161, -29)
+		call_deferred("change_scene", "res://scenes/forest_map.tscn")
 
-func change_scene() -> void:
-	get_tree().change_scene_to_file("res://scenes/forest_map.tscn")
 
-func _ready():
-	$Player.position = Global.spawn_position  # Set player position
+func change_scene(scene_path: String) -> void:
+	get_tree().change_scene_to_file(scene_path)
--- scripts/player.gd
+++ scripts/player.gd
@@ -2,21 +2,30 @@ class_name Player extends CharacterBody2D
 
 var movement_speed = 100.0
 
-
+@onready var animated_sprite = $AnimatedSprite2D
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	pass # Replace with function body.
 
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
+func _physics_process(delta: float) -> void:
+	
 	var direction : Vector2 = Vector2.ZERO
 	direction.x = Input.get_action_strength("right") - Input.get_action_strength("left")
 	direction.y = Input.get_action_strength("down") - Input.get_action_strength("up")
 	
 	velocity = direction * movement_speed
 	
-	pass
-
-func _physics_process(delta: float) -> void:
+	# Update animation based on movement direction
+	if direction.x > 0: # Moving right
+		animated_sprite.play("walk")
+		animated_sprite.flip_h = true
+	elif direction.x < 0: # Moving left
+		animated_sprite.play("walk")
+		animated_sprite.flip_h = false
+	elif direction.y >0 || direction.y<0:
+		animated_sprite.play("walk")
+	else:
+		animated_sprite.play("idle")
+	
 	move_and_slide()
