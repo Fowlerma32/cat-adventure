01de54700357139ac141fede6b6162faa8a3479f
ghxmi <97413957+ghxmi@users.noreply.github.com>
2025-05-06 13:18:14 -0400

    mountain/scences/game

--- diorite_stone.gd
+++ diorite_stone.gd
@@ -3,11 +3,9 @@ extends Area2D
 @export var stone_name := "diorite"
 var already_visited := false
 
-func _on_body_entered(body: Node2D):
+func _on_body_entered(body: Node2D) -> void:
 	if body.is_in_group("Player") and !already_visited:
-		var dialogue = load("res://Dialogues/diorite_info.dialogue")
-		if dialogue:
-			DialogueManager.show_dialogue_balloon(dialogue, "start")
-			await DialogueManager.dialogue_ended
-			SearchQuest.register_stone_visited(stone_name)
-			already_visited = true
+		DialogueManager.show_dialogue_balloon(load("res://Dialogues/Diorite_info.dialogue"), "Diorite_info")
+		await DialogueManager.dialogue_ended
+		SearchQuest.register_stone_visited(stone_name)
+		already_visited = true
